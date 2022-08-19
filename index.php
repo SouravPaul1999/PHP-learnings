67136e15406d940ddf654a3e618306e043bce97a
Sourav Paul <sourav.paul@innoraft.com>
2022-08-19 16:29:13 +0530

    did query by using include method

--- index.php
+++ index.php
@@ -1,28 +1,3 @@
-<?php
-$page=$_GET['q'];
-// var_dump($page);
-if ($page == 2) {
-  header('location: http://sourav_domain.com/login_user.php');
-}
-elseif ($page == 3) {
-  header('location: http://sourav_domain.com/name.php');
-}
-elseif ($page == 4) {
-  header('location: http://sourav_domain.com/photo_upload.php');
-}
-elseif ($page == 5) {
-  header('location: http://sourav_domain.com/mobile.php');
-}
-elseif ($page == 6) {
-  header('location: http://sourav_domain.com/mail.php');
-}
-elseif ($page == 7) {
-  header('location: http://sourav_domain.com/marksheet.php');
-}
-elseif ($page == 8) {
-  header('location: http://sourav_domain.com/pdf_Marksheet.php');
-}
-?>
 
 <html lang="en">
 <head>
@@ -32,11 +7,43 @@ elseif ($page == 8) {
   <title>sourav_website</title>
 </head>
 <body>
-  <h1>Hello World!</h1>
+  <!-- <h1>Hello World!</h1>
 
     <p>This is the landing page of <strong>sourav_domain</strong>.this is my first php in my domain </p>
-    hiiiii
+    hiiiii -->
   
+<?php
+$page=$_GET['q'];
+// var_dump($page);
+if ($page == 2) {
+  // header('location: http://sourav_domain.com/login_user.php');
+  include('./login_user.php');
+}
+elseif ($page == 3) {
+  // header('location: http://sourav_domain.com/name.php');
+  include('./name.php');
+}
+elseif ($page == 4) {
+  // header('location: http://sourav_domain.com/photo_upload.php');
+  include('./photo_upload.php');
+}
+elseif ($page == 5) {
+  // header('location: http://sourav_domain.com/mobile.php');
+  include('./mobile.php');
+}
+elseif ($page == 6) {
+  // header('location: http://sourav_domain.com/mail.php');
+  include('./mail.php');
+}
+elseif ($page == 7) {
+  // header('location: http://sourav_domain.com/marksheet.php');
+  include('./marksheet.php');
+}
+elseif ($page == 8) {
+  // header('location: http://sourav_domain.com/pdf_Marksheet.php');
+  include('./pdf_Marksheet.php');
+}
+?>
 
 </body>
 </html>

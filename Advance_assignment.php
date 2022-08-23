7507ed40df71b3b625dce3877c2bce7c16d07438
Sourav Paul <sourav.paul@innoraft.com>
2022-08-23 17:05:36 +0530

    updating code

--- Advance_assignment.php
+++ Advance_assignment.php
@@ -10,7 +10,7 @@ require('vendor/autoload.php');
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <title>Advance assignment php</title>
+  <title>Document</title>
 </head>
 
 <body>
@@ -35,40 +35,45 @@ require('vendor/autoload.php');
     // var_dump($photo);
 
     $image = 'https://ir-dev-d9.innoraft-sites.com' . $photo->data->attributes->uri->url;
-    // echo "<img src=$image width='' height='' alt='' srcset=''>";
+    // echo "<img src=$image width='' height='' alt='service image' srcset=''>";
     $stitle = ($data['data'][$i]['attributes']['field_secondary_title']['value']);
     // echo $stitle;
     $scontent = ($data['data'][$i]['attributes']['field_services']['value']);
     // echo $scontent;
 
     if ($i % 2 == 0) {
+      echo "<div class='container-fluid'>";
       echo "<div class='row flex-row-reverse flex-row gy-5 justify-content-center'>";
-
+      
       echo "<div class=' col-sm-12 col-md-6  col-xl-4'>";
-
-      echo "<img class='image img-fluid' src='$image'>";
-
+      
+      echo "<img class='image img-fluid' alt='service image' src='$image'>";
+      
       echo "</div>";
-
+      
       echo "<div class=' col-sm-12 col-md-6  col-xl-4'>";
-
+      
       echo "<div class='text-break'>$stitle</div>";
-
-
+      
+      
       echo "<div class='text-break'>$scontent</div>";
-
+      
       echo "<div class='text-lest'><a href='#' class='text-dark text-decoration-none'><button class=' fw-bold btn btn-md btn-white btn-block border border-primary border-color border-2 text-uppercase'  type='submit'>explore more</button></a></div>";
-
+      
+      echo "</div>";
+      
       echo "</div>";
 
       echo "</div>";
     } 
     else {
+      echo "<div class='container-fluid'>";
+
       echo "<div class='row flex-row gy-5 justify-content-center'>";
 
       echo "<div class=' col-sm-12 col-md-6 col-xl-4'>";
 
-      echo "<img class='image img-fluid' src='$image'>";
+      echo "<img class='image img-fluid' alt='service image' src='$image'>";
 
       echo "</div>";
 
@@ -82,6 +87,7 @@ require('vendor/autoload.php');
       
       echo "</div>";
 
+      echo "</div>";
       echo "</div>";
     }
   }

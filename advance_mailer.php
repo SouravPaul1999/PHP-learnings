844c180bbf06b978dd1946c6e1baed23be73bc1e
Sourav Paul <sourav.paul@innoraft.com>
2022-08-25 11:48:36 +0530

    creating mailer css file & did some changes

--- advance_mailer.php
+++ advance_mailer.php
@@ -10,43 +10,7 @@ require('./vendor/autoload.php')
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>mailer</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <style>
-    body {
-      padding-top: 40px;
-      padding-bottom: 40px;
-    }
-
-    .form-signin {
-      max-width: 330px;
-      padding: 15px;
-      margin: 0 auto;
-      color: #017572;
-      font-size: large;
-    }
-
-    .form-signin .form-control {
-      position: relative;
-      height: auto;
-      -webkit-box-sizing: border-box;
-      -moz-box-sizing: border-box;
-      box-sizing: border-box;
-      padding: 10px;
-      font-size: 16px;
-    }
-
-    .form-signin input {
-      margin-bottom: 10px;
-      border-top-left-radius: 0;
-      border-top-right-radius: 0;
-      border-color: #017572;
-    }
-
-    h2 {
-      text-align: center;
-      color: #017572;
-      text-decoration: underline 2px solid #017572;
-    }
-  </style>
+  <link rel="stylesheet" href="mail_style.css">
 </head>
 
 <body>
@@ -104,20 +68,29 @@ require('./vendor/autoload.php')
             /* Set the SMTP port. */
             $mail->Port = 587;
 
+            
             // optional smtp settings, if found som smtp error
             
             // $mail->SMTPOptions = array(
-            //   'ssl' => array(
-            //     'verify_peer' => false,
-            //     'verify_peer_name' => false,
-            //     'allow_self_signed' => true
-            //   )
-            // );
-
-            $mail->setFrom('souravpaul505@gmail.com', 'sourav');
-
+              //   'ssl' => array(
+                //     'verify_peer' => false,
+                //     'verify_peer_name' => false,
+                //     'allow_self_signed' => true
+                //   )
+                // );
+                
+                $mail->setFrom('souravpaul505@gmail.com', 'form submission');
+                
             /* Add a recipient. */
-            $mail->addAddress($email, 'sourav_official_email');
+            $mail->addAddress($email, 'subscriber');
+            
+            // $mail->addCC('admiral@empire.com', 'Fleet Admiral');
+            // $mail->addBCC('luke@rebels.com', 'Luke Skywalker');
+            
+            // adding reply address
+            $mail->addReplyTo('kunal.singh@innoraft.com', 'Admin');
+
+
 
             /* Set the subject. */
             $mail->Subject = 'Thank you';
